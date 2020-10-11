2e2260817c474f0c5ccec9aed19fb58b4e02990d
brian kabiro <briankabiro@gmail.com>
2020-10-11 11:16:40 +0300

    Update validations for balance attribute

--- backend/app/models/account.rb
+++ backend/app/models/account.rb
@@ -2,7 +2,11 @@ class Account < ApplicationRecord
   validates :account_name, presence: true, uniqueness: true
   validates :account_number, presence: true, uniqueness: true
 
-  validates :balance, absence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :balance, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 1000
+  }
 
   has_many :expenses
 end
--- backend/spec/models/account_spec.rb
+++ backend/spec/models/account_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe Account, type: :model do
   context 'validations' do
     it { should validate_presence_of(:account_name) }
     it { should validate_presence_of(:account_number) }
-    it { should validate_numericality_of(:balance) }
+    it 'ensures balance is is an integer greater than 0 and less than 1000' do
+       is_expected.to validate_numericality_of(:balance).only_integer
+        .is_greater_than_or_equal_to(0)
+        .is_less_than_or_equal_to(1000)
+    end
 
     context 'uniqueness' do
       subject { Account.create(account_name: 'runtown', account_number: 12011313) }
