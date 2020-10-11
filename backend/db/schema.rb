c9391276a213e353aa70ad9d69df91ac6ece6ca9
brian kabiro <briankabiro@gmail.com>
2020-10-11 10:47:27 +0300

    Add account foreign key to expenses table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_072046) do
+ActiveRecord::Schema.define(version: 2020_10_11_074450) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "account_name", null: false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2020_10_11_072046) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "account_id", null: false
+    t.index ["account_id"], name: "index_expenses_on_account_id"
   end
 
+  add_foreign_key "expenses", "accounts"
 end
