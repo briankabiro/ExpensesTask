6a5ff196926b713b01b96ff94e87753a46da26db
brian kabiro <briankabiro@gmail.com>
2020-10-11 10:43:05 +0300

    Add specs for the Account model

--- backend/spec/models/account_spec.rb
+++ backend/spec/models/account_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'Account', type: :model do
+RSpec.describe Account, type: :model do
   context 'validations' do
     it { should validate_presence_of(:account_name) }
     it { should validate_presence_of(:account_number) }
-    it { should validate_presence_of(:balance) }
+    it { should validate_numericality_of(:balance) }
+
+    context 'uniqueness' do
+      subject { Account.create(account_name: 'runtown', account_number: 12011313) }
+
+      it { should validate_uniqueness_of(:account_name) }
+      it { should validate_uniqueness_of(:account_number) }
+      it { should validate_absence_of(:balance) }
+    end
   end
 
   context 'associations' do
