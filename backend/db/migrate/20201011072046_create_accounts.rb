ca1e042f6d4a1394daa2635632e6b67109bee0e1
brian kabiro <briankabiro@gmail.com>
2020-10-16 12:16:43 +0300

    Update migration for Account resource
    * use name instead of `account_name`
    * use number instead of `account_number``

--- backend/db/migrate/20201011072046_create_accounts.rb
+++ backend/db/migrate/20201011072046_create_accounts.rb
@@ -1,8 +1,8 @@
 class CreateAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :accounts do |t|
-      t.string :account_name, null: false
-      t.integer :account_number, null: false
+      t.string :name, null: false
+      t.integer :number, null: false
       t.integer :balance, default: 1000, null: false
 
       t.timestamps
