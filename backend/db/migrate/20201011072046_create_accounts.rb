b179634775408234507f5099de94c687e6e56f34
brian kabiro <briankabiro@gmail.com>
2020-10-16 12:58:24 +0300

    Change account_number to string
    * using a string instead of an integer will allow us to save
    numbers that have leading zeroes

--- backend/db/migrate/20201011072046_create_accounts.rb
+++ backend/db/migrate/20201011072046_create_accounts.rb
@@ -2,7 +2,7 @@ class CreateAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :accounts do |t|
       t.string :name, null: false
-      t.integer :number, null: false
+      t.string :number, null: false
       t.integer :balance, default: 1000, null: false
 
       t.timestamps
