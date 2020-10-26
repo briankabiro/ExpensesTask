f2cf4c9c7a9e6bb4316ebfd00095946e0e2b6093
brian kabiro <briankabiro@gmail.com>
2020-10-26 06:54:57 +0300

    Update balance when expense is assigned to account

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -3,4 +3,14 @@ class Expense < ApplicationRecord
   validates :amount, numericality: { greater_than: 0, only_integer: true }
 
   belongs_to :account
+
+  after_commit :update_account, on: :create
+
+  def update_account
+    account = Account.find(account_id)
+
+    new_balance = account.balance - amount
+
+    account.update!(balance: new_balance)
+  end
 end
