1498844997189884cc0068b5f9e1acba59284c20
brian kabiro <briankabiro@gmail.com>
2020-10-26 06:52:09 +0300

    Add specs for Account model

--- backend/spec/models/account_spec.rb
+++ backend/spec/models/account_spec.rb
@@ -2,24 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Account, type: :model do
   context 'validations' do
-    it { should validate_presence_of(:account_name) }
-    it { should validate_presence_of(:account_number) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:number) }
+
     it 'ensures balance is is an integer greater than 0 and less than 1000' do
        is_expected.to validate_numericality_of(:balance).only_integer
         .is_greater_than_or_equal_to(0)
         .is_less_than_or_equal_to(1000)
     end
 
-    context 'uniqueness' do
-      subject { Account.create(account_name: 'runtown', account_number: 12011313) }
-
-      it { should validate_uniqueness_of(:account_name) }
-      it { should validate_uniqueness_of(:account_number) }
-      it { should validate_absence_of(:balance) }
+    context 'associations' do
+      it { should have_many(:expenses) }
     end
-  end
 
-  context 'associations' do
-    it { should have_many(:expenses) }
+    context 'uniqueness' do
+      subject { Account.create(name: 'runtown', number: '123456bm') }
+
+      it { should validate_uniqueness_of(:name) }
+      it { should validate_uniqueness_of(:number) }
+    end
+
+    context 'balance' do
+      let(:account) { Account.new(name: 'runtown', number: '123456bm') }
+
+      it 'raises error when trying to set initial balance' do
+        account.update(balance: 100)
+        account.save!
+
+        expect(account.balance).to eq(1000)
+      end
+
+      it 'raises error when balance is less than 0' do
+        account.save!
+
+        expect { account.update!(balance: -10) }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
   end
 end
